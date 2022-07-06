38611dfe5faa3171205f9f32ebc28e600c21cd63
Demo (Firas) <58010700+demofiras@users.noreply.github.com>
2022-07-06 09:55:23 -0700

    Add some sample code in a for loop

---
+++ code/example1.py
@@ -0,0 +1,10 @@
+# Hundreds of lines of code above this
+
+urls = []
+for path in paths:
+  url = 'https://'
+  url += domain
+  url += path
+  urls.append(url)
+  
+# Hundreds of lines of code below this
